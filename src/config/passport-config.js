12e2647ddba90167bf96ad9820c29de100b57f8d
Lola <lmariemeehan@gmail.com>
2020-02-09 15:06:54 -0800

    Started passport config file

---
+++ src/config/passport-config.js
@@ -0,0 +1,22 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("../db/models").User;
+const authHelper = require("../auth/helpers");
+
+module.exports = {
+  init(app){
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    passport.use(new LocalStrategy({
+      usernameField: "email"
+    }, (email, password, done) => {
+      User.findOne({
+        where: { email }
+      })
+      .then((user) => {
+        if(!user || !authHelper.comparePass())
+      })
+    }))
+  }
+}
