1e08420e17476adca17d9cfd9ec57b0657747251
Lola <lmariemeehan@gmail.com>
2020-02-15 17:11:44 -0800

    Fixed express validator and edited sign up page

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const path = require("path");
 const viewsFolder = path.join(__dirname, "..", "views");
 const bodyParser = require("body-parser");
-const { check, validationResult } = require('express-validator');
+const expressValidator = require('express-validator');
 const session = require("express-session");
 const flash = require("express-flash");
 const logger = require("morgan");
