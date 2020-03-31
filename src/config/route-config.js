0a2b9012c5915dd355f51fa91c83083f58ec9686
Lola <lmariemeehan@gmail.com>
2020-03-30 21:10:42 -0700

    Added appointments to the main route config file

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -4,9 +4,11 @@ module.exports = {
     const inrRoutes = require("../routes/inrs");
     const medicationRoutes = require("../routes/medications");
     const userRoutes = require("../routes/users");
+    const appointmentRoutes = require("../routes/appointments");
     app.use(staticRoutes);
     app.use(inrRoutes);
     app.use(medicationRoutes);
     app.use(userRoutes);
+    app.use(appointmentRoutes);
   }
 }
